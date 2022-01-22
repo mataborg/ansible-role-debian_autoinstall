7bcc38a96bda358e67e6769ffeabd8c401f4006e
mataborg <97360301+mataborg@users.noreply.github.com>
2022-01-22 21:28:56 +0000

    Fix hosts issue for ProxMox APT install

--- latecommands.sh
+++ latecommands.sh
@@ -1,13 +1,14 @@
+sed 's/127.0.1.1/192.168.2.19/' /etc/hosts
 apt update
 apt full-upgrade -y
 apt autoremove -y
 echo "deb [arch=amd64] http://download.proxmox.com/debian/pve bullseye pve-no-subscription" > /etc/apt/sources.list.d/pve-install-repo.list
 wget https://enterprise.proxmox.com/debian/proxmox-release-bullseye.gpg -O /etc/apt/trusted.gpg.d/proxmox-release-bullseye.gpg
 sha512sum /etc/apt/trusted.gpg.d/proxmox-release-bullseye.gpg
-# apt update
-# apt install proxmox-ve postfix open-iscsi -y
-# apt autoremove -y
-# apt remove linux-image-amd64 'linux-image-5.10*' -y
-# apt remove os-prober -y
-# apt clean
-# update-grub
+apt update
+apt install proxmox-ve postfix open-iscsi -y
+apt autoremove -y
+apt remove linux-image-amd64 'linux-image-5.10*' -y
+apt remove os-prober -y
+apt clean
+update-grub
