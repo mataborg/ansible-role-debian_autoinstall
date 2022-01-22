00d3462136364eaf98b6797eb85c642b00fec7a2
mataborg <97360301+mataborg@users.noreply.github.com>
2022-01-22 16:30:31 +0000

    Syntax

--- latecommands.sh
+++ latecommands.sh
@@ -2,7 +2,9 @@ echo "deb [arch=amd64] http://download.proxmox.com/debian/pve bullseye pve-no-su
 wget https://enterprise.proxmox.com/debian/proxmox-release-bullseye.gpg -O /etc/apt/trusted.gpg.d/proxmox-release-bullseye.gpg
 sha512sum /etc/apt/trusted.gpg.d/proxmox-release-bullseye.gpg
 apt update
-apt full-upgrade -y
+apt upgrade -y
 apt install proxmox-ve postfix open-iscsi -y
-# apt remove os-prober -y
+apt remove os-prober -y
 apt remove linux-image-amd64 'linux-image-5.10*' -y
+apt autoremove -y
+apt clean
