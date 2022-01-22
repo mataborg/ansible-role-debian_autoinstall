8a79fea4d7a893daa0931d7afc234253682c6753
mataborg <97360301+mataborg@users.noreply.github.com>
2022-01-22 19:32:56 +0000

    PVE Testing

--- latecommands.sh
+++ latecommands.sh
@@ -5,9 +5,9 @@ echo "deb [arch=amd64] http://download.proxmox.com/debian/pve bullseye pve-no-su
 wget https://enterprise.proxmox.com/debian/proxmox-release-bullseye.gpg -O /etc/apt/trusted.gpg.d/proxmox-release-bullseye.gpg
 sha512sum /etc/apt/trusted.gpg.d/proxmox-release-bullseye.gpg
 apt update
-apt install proxmox-ve postfix open-iscsi -y
-apt autoremove -y
-apt remove linux-image-amd64 'linux-image-5.10*' -y
-apt remove os-prober -y
-apt clean
-update-grub
+# apt install proxmox-ve postfix open-iscsi -y
+# apt autoremove -y
+# apt remove linux-image-amd64 'linux-image-5.10*' -y
+# apt remove os-prober -y
+# apt clean
+# update-grub
