3cbe03e385c44ad642f6f3cded1ec97f7142ff5a
mataborg <97360301+mataborg@users.noreply.github.com>
2022-01-22 23:55:39 +0000

    Update latecommands.sh

--- latecommands.sh
+++ latecommands.sh
@@ -4,6 +4,9 @@ apt full-upgrade -y
 sed -i 's/127.0.1.1/192.168.2.19/' /etc/hosts
 echo "deb [arch=amd64] http://download.proxmox.com/debian/pve bullseye pve-no-subscription" > /etc/apt/sources.list.d/pve-install-repo.list
 wget https://enterprise.proxmox.com/debian/proxmox-release-bullseye.gpg -O /etc/apt/trusted.gpg.d/proxmox-release-bullseye.gpg
+apt update
 apt install proxmox-ve postfix open-iscsi -y
+apt autoremove -y
 apt remove os-prober -y
+apt clean
 update-grub
