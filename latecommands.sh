42fda29c15a00f2409c979e46d386127f2b3b71b
mataborg <97360301+mataborg@users.noreply.github.com>
2022-01-22 19:48:28 +0000

    testing

--- latecommands.sh
+++ latecommands.sh
@@ -4,7 +4,7 @@ apt autoremove -y
 echo "deb [arch=amd64] http://download.proxmox.com/debian/pve bullseye pve-no-subscription" > /etc/apt/sources.list.d/pve-install-repo.list
 wget https://enterprise.proxmox.com/debian/proxmox-release-bullseye.gpg -O /etc/apt/trusted.gpg.d/proxmox-release-bullseye.gpg
 sha512sum /etc/apt/trusted.gpg.d/proxmox-release-bullseye.gpg
-apt update
+# apt update
 # apt install proxmox-ve postfix open-iscsi -y
 # apt autoremove -y
 # apt remove linux-image-amd64 'linux-image-5.10*' -y
