b4a4198b5956ba5536ea9535639d29934f8f47fc
mataborg <97360301+mataborg@users.noreply.github.com>
2022-01-22 23:40:55 +0000

    Update latecommands.sh

--- latecommands.sh
+++ latecommands.sh
@@ -6,3 +6,4 @@ echo "deb [arch=amd64] http://download.proxmox.com/debian/pve bullseye pve-no-su
 wget https://enterprise.proxmox.com/debian/proxmox-release-bullseye.gpg -O /etc/apt/trusted.gpg.d/proxmox-release-bullseye.gpg
 apt install proxmox-ve postfix open-iscsi -y
 apt remove os-prober -y
+update-grub
