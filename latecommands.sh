c27f948aaf1f18707133ebce92a9107fcb771113
mataborg <97360301+mataborg@users.noreply.github.com>
2022-01-23 18:18:35 +0000

    systemd syntax error

--- latecommands.sh
+++ latecommands.sh
@@ -9,12 +9,11 @@ sed -i 's/127.0.1.1/192.168.2.19/' /etc/hosts
 
 # Download systemd component and PVE install script
 curl -sSL 'https://raw.githubusercontent.com/mataborg/ansible-role-debian_autoinstall/main/firstboot.sh' -o "/root/firstboot.sh"
-curl -sSL 'https://raw.githubusercontent.com/mataborg/ansible-role-debian_autoinstall/main/firstboot.service' -o "/etc/systemd/system/"
+curl -sSL 'https://raw.githubusercontent.com/mataborg/ansible-role-debian_autoinstall/main/firstboot.service' -o "/etc/systemd/system/firstboot.service"
 
 # Enable systemd to run PVE install script on boot
 chmod 744 /root/firstboot.sh
 chmod 664 /etc/systemd/system/firstboot.service
-systemctl daemon-reload
 systemctl enable firstboot.service
 
 }
