9b88bc02caf8aa7608be56b02f5d9b45ecbd2957
mataborg <97360301+mataborg@users.noreply.github.com>
2022-01-23 17:56:15 +0000

    Create systemd method to install PVE on first Debian system reboot

--- firstboot.sh
+++ firstboot.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
 # https://stackoverflow.com/questions/12973777/how-to-run-a-shell-script-at-startup/12973826#12973826
+### First system boot PVE install script ###
 
 firstboot() {
 
+# Install PVE
 sed -i 's/127.0.1.1/192.168.2.19/' /etc/hosts
+echo "deb [arch=amd64] http://download.proxmox.com/debian/pve bullseye pve-no-subscription" > /etc/apt/sources.list.d/pve-install-repo.list
+wget https://enterprise.proxmox.com/debian/proxmox-release-bullseye.gpg -O /etc/apt/trusted.gpg.d/proxmox-release-bullseye.gpg
 apt update
-apt install proxmox-ve postfix open-iscsi -y
 apt full-upgrade -y
-apt autoremove -y
+apt install proxmox-ve -y
 apt remove os-prober -y
+apt autoremove -y
 update-grub
 
-}
-firstboot
+# Cleaning house
+rm /root/firstboot.sh
+rm /etc/systemd/system/firstboot.service
+systemctl daemon-reload
+
 # Delete me
-rm $0
+rm -- "$0"
+
+}
+
+firstboot
+
+
--- latecommands.sh
+++ latecommands.sh
@@ -1,12 +1,22 @@
+# https://linuxconfig.org/how-to-automatically-execute-shell-script-at-startup-boot-on-systemd-linux 
+# https://www.mauras.ch/systemd-run-it-last.html 
 ### Post-Install d-i late_commands script ###
-apt update
-apt full-upgrade -y
+
+latecommands() {
+
+# Modify Hosts file
 sed -i 's/127.0.1.1/192.168.2.19/' /etc/hosts
-echo "deb [arch=amd64] http://download.proxmox.com/debian/pve bullseye pve-no-subscription" > /etc/apt/sources.list.d/pve-install-repo.list
-wget https://enterprise.proxmox.com/debian/proxmox-release-bullseye.gpg -O /etc/apt/trusted.gpg.d/proxmox-release-bullseye.gpg
-apt update
-apt install proxmox-ve postfix open-iscsi -y
-apt full-upgrade -y
-apt autoremove -y
-apt remove os-prober -y
-update-grub
+
+# Download systemd component and PVE install script
+curl -sSL 'https://raw.githubusercontent.com/mataborg/ansible-role-debian_autoinstall/main/firstboot.sh' -o "/root/firstboot.sh"
+curl -sSL 'https://raw.githubusercontent.com/mataborg/ansible-role-debian_autoinstall/main/firstboot.service' -o "/etc/systemd/system/"
+
+# Enable systemd to run PVE install script on boot
+chmod 744 /root/firstboot.sh
+chmod 664 /etc/systemd/system/firstboot.service
+systemctl daemon-reload
+systemctl enable firstboot.service
+
+}
+
+latecommands
