2840aab945a0fe6927b2a971ebe4963db3eccfd0
Logan Cowan <97360301+mataborg@users.noreply.github.com>
2023-07-28 13:43:11 -0400

    Add PVE Kernel, Debian 12.1
    Bumped to Debian 12.1 Bookworm

--- firstboot.sh
+++ firstboot.sh
@@ -4,13 +4,14 @@
 
 firstboot() {
 
-    # Install PVE
+    # Install PVE/Remove shipped Debian Kernel
     sed -i 's/127.0.1.1/192.168.2.19/' /etc/hosts
-    echo "deb [arch=amd64] http://download.proxmox.com/debian/pve bullseye pve-no-subscription" > /etc/apt/sources.list.d/pve-install-repo.list
-    wget https://enterprise.proxmox.com/debian/proxmox-release-bullseye.gpg -O /etc/apt/trusted.gpg.d/proxmox-release-bullseye.gpg
+    # echo "deb [arch=amd64] http://download.proxmox.com/debian/pve bookworm pve-no-subscription" > /etc/apt/sources.list.d/pve-install-repo.list
+    # wget https://enterprise.proxmox.com/debian/proxmox-release-bookworm.gpg -O /etc/apt/trusted.gpg.d/proxmox-release-bookworm.gpg
     apt update
     apt full-upgrade -y
     apt install proxmox-ve -y
+    apt remove linux-image-amd64 'linux-image-6.1*' -y
     apt remove os-prober -y
     apt autoremove -y
     update-grub
--- latecommands.sh
+++ latecommands.sh
@@ -7,6 +7,12 @@ latecommands() {
     # Modify Hosts file
     sed -i 's/127.0.1.1/192.168.2.19/' /etc/hosts
 
+    # Add PVE Repository to install PVE Kernel package
+    echo "deb [arch=amd64] http://download.proxmox.com/debian/pve bookworm pve-no-subscription" > /etc/apt/sources.list.d/pve-install-repo.list
+    wget https://enterprise.proxmox.com/debian/proxmox-release-bookworm.gpg -O /etc/apt/trusted.gpg.d/proxmox-release-bookworm.gpg
+    apt update
+    apt install pve-kernel-6.2 -y
+
     # Download systemd component and PVE install script
     curl -sSL 'https://raw.githubusercontent.com/mataborg/ansible-role-debian_autoinstall/main/firstboot.sh' -o "/root/firstboot.sh"
     curl -sSL 'https://raw.githubusercontent.com/mataborg/ansible-role-debian_autoinstall/main/firstboot.service' -o "/etc/systemd/system/firstboot.service"
